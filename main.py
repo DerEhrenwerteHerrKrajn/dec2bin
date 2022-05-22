0f7b917479141e96e211967be3d41a03c3b4c304
DerEhrenwerteHerrKrajn <90752878+DerEhrenwerteHerrKrajn@users.noreply.github.com>
2022-05-22 23:10:21 +0200

    Add files via upload
    Bitshift operations and variable button number added

--- main.py
+++ main.py
@@ -10,8 +10,15 @@ leds = LEDBoard(18, 23, 24, 25, pwm=True)
 
 # Klasse für das Hauptfenster
 class MyWindow(QMainWindow):
+    nrButtons = 4 
+    decMax = 0
+    for i in range(nrButtons):
+        decMax += 2**i
+    
+
     def __init__(self): 
 
+        
         super().__init__()
         self.setMinimumSize(QSize(250, 120)) 
         self.setWindowTitle('Dezimalumwandler Krajina Luka') 
@@ -24,7 +31,7 @@ class MyWindow(QMainWindow):
 
         self.binSlider = QSlider(Qt.Horizontal)
         self.binSlider.setGeometry(30, 40, 150, 50)
-        self.binSlider.setRange(0, 15)
+        self.binSlider.setRange(0, self.decMax)
         self.binSlider.setPageStep(1) 
         self.binSlider.setTickPosition(2)
         hlayoutSlider.addWidget(self.binSlider)
@@ -37,7 +44,9 @@ class MyWindow(QMainWindow):
         #self.bitlabels = ['zahl8', 'zahl4', 'zahl2', 'zahl1']
         #for self.bitlabel in self.bitlabels:
         #    hlayoutLabels.addWidget(self.bitlabel)
-        self.zahlen = [QLabel('8', self), QLabel('4', self), QLabel('2', self), QLabel('1', self)]
+        self.zahlen = [] 
+        for i in range(self.nrButtons):
+            self.zahlen.append(QLabel(str(2**(self.nrButtons - i - 1)), self)) 
         for x in self.zahlen:
             hlayoutLabels.addWidget(x)
 
@@ -59,16 +68,12 @@ class MyWindow(QMainWindow):
     def checker(self):
         labelsValue = (self.binSlider.value())
         print("Start: " + str(labelsValue))
-        on = "background-color: rgb(0, 180, 0)"
-        off = "background-color: rgb(180, 0, 0)"
-        for i in range(4):
-            div = 2 ** (3 - i)
-            self.zahlen[i].setStyleSheet(off)
-            if labelsValue >= div:
-                    self.zahlen[i].setStyleSheet(on)
-                    leds[i].on()
-                    labelsValue -= div
-        print("________")
+        for i in range(self.nrButtons):
+            self.zahlen[self.nrButtons - 1 - i].setStyleSheet("background-color: rgb(180, 0, 0)")
+            leds[i].off()
+            if (labelsValue & 1<<i):
+                self.zahlen[self.nrButtons - 1 - i].setStyleSheet("background-color: rgb(0, 180, 0)")
+                leds[i].on()
 
 
 app = QtWidgets.QApplication([])
